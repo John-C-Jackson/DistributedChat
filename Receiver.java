6b53a7d61c5256465caf7b7023a8c3174706ce61
cv456 <cv456@nau.edu>
2020-02-27 17:01:29 -0700

    updating receiver
    questions on receiver

--- Receiver.java
+++ Receiver.java
@@ -1,5 +1,19 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class Receiver
 {
+  int inImportNumber;
+  String message;
 
+  /*
+  somewhere here do we unload the message to send as a string?
+  and then send it to ReceiverWorker ??
+  or do we send ReceiverWorker a object and then ReceiverWorker unpacks? 
+  **/
+
+  Runnable runHolder = new ReceiverWorker(inImportNumber, message);
+  Thread threadHolder = new Thread(runHolder);
+  threadHolder.start();
 }
--- ReceiverWorker.java
+++ ReceiverWorker.java
@@ -1,10 +1,22 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 
 public class ReceiverWorker implements Runnable
 {
 
+  ReceiverWorker(int inImportNumber , String message)
+  {
+    this.inImportNumber = inImportNumber;
+    this.message = message;
+  }
+
     @Override
     public void run ()
     {
+      
+
         // TODO Auto-generated method stub
 
     }
