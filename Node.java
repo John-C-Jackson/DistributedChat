feae97f5383587b75f6f3fa20317e682897d7e9d
John Jackson <jcj52@nau.edu>
2020-03-01 11:12:55 -0700

    Node vars, main

--- Node.java
+++ Node.java
@@ -1,5 +1,24 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class Node
 {
+	Sender sender = new Sender;
+	Receiver receiver = new Receiver;
+	ArrayList<Node> nodeList = new ArrayList<Node>;
+
+    public static void main(String[] args) throws IOException
+    {
+    	if (args.length != 2) {
+            System.err.println(
+                "Usage: java Node <host name> <port number> <your name>");
+            System.exit(1);
+        }
+    
+    	String hostName = args[0];
+    	int portNumber = Integer.parseInt(args[1]);
+    	String name = args[2];    
+    }
 
 }
